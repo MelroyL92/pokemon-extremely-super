d0c7281ddca9cf4f270b1b29a407ba3c3e1f15ef
MelroyL92 <melroy_lingsma@live.nl>
2023-12-15 16:43:34 +0100

    First part of grass class done

---
+++ src/GrassPokemon.java
@@ -0,0 +1,22 @@
+public class GrassPokemon extends Pokemon{
+
+    public GrassPokemon(String name, int level, int hp, String food, String sound) {
+        super(name, level, hp, food, sound);
+    }
+
+    public void leechSeed(Pokemon name, Pokemon enemy){
+
+    }
+
+    public void leaveBlade(Pokemon name, Pokemon enemy){
+
+    }
+
+    public void leafStorm(Pokemon name, Pokemon enemy){
+
+    }
+
+    public void solarBeam(Pokemon name, Pokemon enemy){
+
+    }
+}
