819830efe1a7584bdb11ac23a6b802f82593d79c
MelroyL92 <melroy_lingsma@live.nl>
2023-12-15 16:44:01 +0100

    First part of water class done

---
+++ src/WaterPokemon.java
@@ -0,0 +1,22 @@
+public class WaterPokemon extends Pokemon {
+
+    public WaterPokemon(String name, int level, int hp, String food, String sound){
+        super(name, level, hp, food, sound);
+
+    }
+
+    void surf(Pokemon name, Pokemon enemy){
+
+    }
+
+    void hydroPump(Pokemon name, Pokemon enemy){
+
+    }
+    void hydroCanon(Pokemon name, Pokemon enemy){
+
+    }
+
+    public void rainDance(Pokemon name, Pokemon enemy){
+
+    }
+}
