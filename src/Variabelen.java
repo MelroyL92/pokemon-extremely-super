905ea13ad2d88fd2e2088481005fb67661666c3e
MelroyL92 <melroy_lingsma@live.nl>
2023-12-15 16:45:46 +0100

    first part done

--- src/Variabelen.java
+++ src/Variabelen.java
@@ -6,15 +6,10 @@ import java.util.List;
 public class Variabelen {
     /*deze komen meerdere keren voor*/
     type;/*""*/
-    List<String> attacks = Arrays.asList(/*voer de aanvallen hier in*/);
+
     name;/*""*/
 
     /*deze variabelen komen eenmaal voor*/
     List<Pokemon> pokemons;
-    int level;
-    int hp;
-    food;/*""*/
-    sound;/*""*/
-    town;/*""*/
 
 }
