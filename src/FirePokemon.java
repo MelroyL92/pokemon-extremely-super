df804ea15c74dd3a0c35103b145145e70458e749
MelroyL92 <melroy_lingsma@live.nl>
2023-12-15 16:42:39 +0100

    First part of fire done

---
+++ src/FirePokemon.java
@@ -0,0 +1,23 @@
+public class FirePokemon extends Pokemon {
+
+    public FirePokemon (String name, int level, int hp, String food, String sound) {
+       super(name, level, hp, food, sound);
+    }
+
+    void inferno(Pokemon name, Pokemon enemy){
+
+    }
+
+    void flameThrower(Pokemon name, Pokemon enemy){
+
+    }
+
+    void pyroBall(Pokemon name, Pokemon enemy){
+
+    }
+
+    void fireLash(Pokemon name, Pokemon enemy){
+
+    }
+
+}
