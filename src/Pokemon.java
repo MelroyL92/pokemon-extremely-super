93fabc3cacfaa9c61e282019f7298c5c3acbb9d3
MelroyL92 <melroy_lingsma@live.nl>
2023-12-15 16:43:01 +0100

    First part of super class pokemon done

---
+++ src/Pokemon.java
@@ -0,0 +1,53 @@
+import java.util.List;
+
+public abstract class Pokemon {
+
+        private String name;
+        private int level;
+        private int hp;
+        private String food;
+        private String sound;
+        private String type;
+
+        public Pokemon (String name, int level, int hp, String food, String sound, String type) {
+            this.name = name;
+            this.level = level;
+            this.hp = hp;
+            this.food = food;
+            this.sound = sound;
+            this.type = type;
+        }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getName(){
+            return name;
+        }
+
+        public int getLevel(){
+            return level;
+        }
+
+        public int getHp(){
+            return hp;
+        }
+
+        public void setHp(int hp) {
+            this.hp = hp;
+        }
+
+    public String getFood(){
+            return food;
+        }
+
+        public String getSound(){
+            return sound;
+        }
+
+
+
+
+    }
+
